24ff9e617936c620a7671433817303562d4548eb
12Ivan03 <144702214+12Ivan03@users.noreply.github.com>
2025-06-24 23:11:42 +0200

    more tests

--- routine.c
+++ routine.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   routine.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ipavlov <ipavlov@student.codam.nl>         +#+  +:+       +#+        */
+/*   By: penchoivanov <penchoivanov@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/28 10:47:31 by ipavlov           #+#    #+#             */
-/*   Updated: 2025/06/24 19:04:03 by ipavlov          ###   ########.fr       */
+/*   Updated: 2025/06/24 23:10:28 by penchoivano      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,21 @@ int	gorge_oneself(t_philo *ph)
 	return (0);
 }
 
+void	staggering_meassures(t_philo *ph)
+{
+	if (ph->manager->nbr_philo % 2 == 1 && ph->manager->nbr_philo < 51 \
+											&& ph->philo_id % 2 == 1)
+		usleep(ph->time_to_eat * 1000 / 2);
+	else if (ph->manager->nbr_philo % 2 == 1 && ph->philo_id % 2 == 1)
+		usleep(400);
+	if (ph->manager->nbr_philo % 2 == 0 && ph->manager->nbr_philo >= 50 \
+										&& ph->philo_id % 2 == 1)
+		usleep(200);
+	else if (ph->manager->nbr_philo % 2 == 0 && ph->manager->nbr_philo < 50 \
+											&& ph->philo_id % 2 == 1)
+		usleep(100);
+}
+
 void	*routine(void *catch_philo)
 {
 	t_philo		*ph;
@@ -33,18 +48,20 @@ void	*routine(void *catch_philo)
 	while (!global_grim_dead_f(ph->manager) && \
 			!philo_meal_allowence(ph) && !philo_dead_f(ph))
 	{
-		if (ph->manager->nbr_philo % 2 == 1 && ph->manager->nbr_philo < 51 \
-											&& ph->philo_id % 2 == 1)
-			usleep(ph->time_to_eat * 1000 / 2);
-		else if (ph->manager->nbr_philo % 2 == 1 && ph->philo_id % 2 == 1)
-			usleep(400);
-		if (ph->manager->nbr_philo % 2 == 0 && ph->manager->nbr_philo >= 50 \
-											&& ph->philo_id % 2 == 1)
-			usleep(200);
-		else if (ph->manager->nbr_philo % 2 == 0 && ph->manager->nbr_philo < 50 \
-												&& ph->philo_id % 2 == 1)
-			usleep(100);
+		staggering_meassures(ph);
 		gorge_oneself(ph);
 	}
 	return (NULL);
 }
+
+// if (ph->manager->nbr_philo % 2 == 1 && ph->manager->nbr_philo < 51 \
+// 									&& ph->philo_id % 2 == 1)
+// 	usleep(ph->time_to_eat * 1000 / 2);
+// else if (ph->manager->nbr_philo % 2 == 1 && ph->philo_id % 2 == 1)
+// 	usleep(400);
+// if (ph->manager->nbr_philo % 2 == 0 && ph->manager->nbr_philo >= 50 \
+// 									&& ph->philo_id % 2 == 1)
+// 	usleep(200);
+// else if (ph->manager->nbr_philo % 2 == 0 && ph->manager->nbr_philo < 50 \
+// 										&& ph->philo_id % 2 == 1)
+// 	usleep(100);
